d077f41c07788b3612be6d94654d56d3710b4a78
Teja Dandu <teja.dandu484@gmail.com>
2023-01-03 22:27:17 +0530

    Intial setup done

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const {PORT} = require('./config/serverConfig');
+
+
+const setupAndStartServer = async () => {
+    // Create the express object
+
+    const app = express();
+
+    // middleware
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.listen(PORT, () => {
+        console.log(`Server started at ${PORT}`);
+
+    });
+}
+
+setupAndStartServer();
